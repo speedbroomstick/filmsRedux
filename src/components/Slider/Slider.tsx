56df84b664b856a6253db0e0a4b430837d8bb19f
LeonidGovorec <lenagovorec58@gmail.com>
2024-01-24 19:39:29 +0300

    Swiper Done

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,38 +1,22 @@
-import { useEffect, useState } from "react";
 import { ISlider } from "../../types/films.types";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.css';
 import FilmCard from "../MovieGroup/FilmCard/FilmCard";
 import "./slider.scss";
 
 export default function Slider({ films }: ISlider) {
 
-    const [indexFilm, setIndexFilm] = useState(0);
-    const [copyFilms, setCopyFilms] = useState(films.slice(0, 4));
-
-    useEffect(() => {
-        let i = 1;
-        if (indexFilm !== films.length - 1) {
-            const timeoutId = setTimeout(() => {
-                setIndexFilm(indexFilm + 1);
-                copyFilms[i].poster_path = films[indexFilm].poster_path;
-               // setCopyFilms([...copyFilms])
-                i++;
-            }, 1000);
-            return () => {
-                clearTimeout(timeoutId);
-            };
-        }
-        else{
-            setIndexFilm(0);
-        }
-    }, [indexFilm, films.length]);
-
     return (
         <div className="wraperSlider">
             <h1>Comedy</h1>
             <div className="slider">
-            {[0, 1, 2, 3].map((i, index) => (
-                 <FilmCard key={i} poster_path={copyFilms[index].poster_path} scale={{ width: 14, height: 20 }} margin={1} />
-            ))}
+                <Swiper
+                    slidesPerView={4}
+                >
+                    {films.map((film)=>(
+                            <SwiperSlide><FilmCard key={film.id} poster_path={film.poster_path} scale={{ width: 14, height: 20 }} margin={1} /></SwiperSlide>
+                    ))}
+                </Swiper>
             </div>
         </div>
     )
